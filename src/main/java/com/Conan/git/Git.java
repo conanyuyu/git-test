527a8294b5031e9a088b0d5cf523c2974049bae1
Conan <249192399@qq.com>
2021-09-05 10:13:06 +0800

    second commit

--- src/main/java/com/Conan/git/Git.java
+++ src/main/java/com/Conan/git/Git.java
@@ -3,5 +3,7 @@ package com.Conan.git;
 public class Git {
     public static void main(String[] args) {
         System.out.println("heloo git!");
+        System.out.println("heloo github!");
+        System.out.println("heloo githhh!");
     }
 }
